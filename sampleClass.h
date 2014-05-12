f1842f69c0086044da7c811ac2acdd6f75029861
Nasir <nasirlambton@gmail.com>
2014-05-12 18:30:08 -0400

    First commit

--- sampleClass.h
+++ sampleClass.h
@@ -1,14 +1,13 @@
 //
-//  ViewController.h
+//  sampleClass.h
 //  nasir
 //
 //  Created by User1 on 2014-05-12.
 //  Copyright (c) 2014 GroupA. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+@interface sampleClass : NSObject
 
-
-@end 
+@end
--- sampleClass.m
+++ sampleClass.m
@@ -1,14 +1,13 @@
 //
-//  ViewController.h
+//  sampleClass.m
 //  nasir
 //
 //  Created by User1 on 2014-05-12.
 //  Copyright (c) 2014 GroupA. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "sampleClass.h"
 
-@interface ViewController : UIViewController
+@implementation sampleClass
 
-
-@end 
+@end

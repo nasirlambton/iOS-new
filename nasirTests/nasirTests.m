276688f4551ee9a71936a0424cbcb90128dde5aa
Nasir <nasirlambton@gmail.com>
2014-05-12 18:21:19 -0400

    Initial Commit

---
+++ nasirTests/nasirTests.m
@@ -0,0 +1,34 @@
+//
+//  nasirTests.m
+//  nasirTests
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 GroupA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface nasirTests : XCTestCase
+
+@end
+
+@implementation nasirTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
